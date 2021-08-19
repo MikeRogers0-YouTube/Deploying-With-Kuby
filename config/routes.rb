33e644a57ccd4254ee2cbfea361c976747b3deb7
Mike Rogers <me@mikerogers.io>
2021-08-18 21:28:19 -0500

    bundle exec standardrb --fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :posts
 
-  root to: redirect('/posts')
+  root to: redirect("/posts")
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
--- spec/support/faker.rb
+++ spec/support/faker.rb
@@ -1,1 +1,1 @@
-require 'faker'
+require "faker"
